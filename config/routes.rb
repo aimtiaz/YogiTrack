2cfb977c3139db779bb5b1b1ea1f74512d2aad8a
aimtiaz <aimtiaz3@uh.edu>
2017-04-01 23:39:06 -0500

    Added login

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :private_lessons
   resources :equipment_checkouts
   resources :program_rosters
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170329045703) do
+ActiveRecord::Schema.define(version: 20170401211856) do
 
   create_table "classroom_statuses", force: :cascade do |t|
     t.string   "classroom_status", limit: 4000
@@ -138,5 +138,23 @@ ActiveRecord::Schema.define(version: 20170329045703) do
     t.datetime "created_at",                       null: false
     t.datetime "updated_at",                       null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  limit: 4000, default: "", null: false
+    t.string   "encrypted_password",     limit: 4000, default: "", null: false
+    t.string   "reset_password_token",   limit: 4000
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          limit: 4,    default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip",     limit: 4000
+    t.string   "last_sign_in_ip",        limit: 4000
+    t.datetime "created_at",                                       null: false
+    t.datetime "updated_at",                                       null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
